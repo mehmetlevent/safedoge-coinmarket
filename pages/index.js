2d3cade7d2423f0a7a9c2d6ffb5b37f7e4d4b71a
mehmetlevent <m.levent.arik@gmail.com>
2021-06-06 23:35:46 +0300

    init

---
+++ pages/index.js
@@ -0,0 +1,76 @@
+import Head from 'next/head'
+
+import styles from '../styles/Home.module.css'
+import CoinGecko from 'coingecko-api';
+const coinGeckoClient = new CoinGecko();
+
+
+export default function Home(props) {
+  const { data } = props.result;
+  const formatDollar = (number, maximumSignificantDigits) =>
+  new Intl.NumberFormat(
+    'en-US',
+    {
+      style: 'currency',
+      currency: 'usd',
+      maximumSignificantDigits
+    }
+  )
+  .format(number);
+  const formatPercent = number =>
+    
+  `${new Number(number).toFixed(2)}%`
+  
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>SafeDoge MarketCap</title>
+        
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1> SAFEDOGE MarketCap</h1>
+
+      <table className ="table">
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>24H Change</th>
+            <th>Price</th>
+            <th>Market Cap</th>
+          </tr>  
+        </thead>
+        <tbody>
+          {data.map(coin => (
+            <tr key={coin.id}>
+              <td>
+                <img 
+                  src={coin.image}
+                  style={{width: 25, height: 25, marginRight:10}} />
+                {coin.symbol.toUpperCase()}</td>
+              <td>
+                <span
+                  className = {coin.price_change_percentage_24h>0 ? (
+                    'text-success'
+                    ) : 'text-danger'}>
+              {formatPercent(coin.price_change_percentage_24h)} </span> </td>
+              <td>{formatDollar(coin.current_price,20)}</td>
+              <td>{formatDollar(coin.market_cap,12)}</td>
+              </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+export async function getServerSideProps(context) {
+  const params = {
+    order: CoinGecko.ORDER.MARKET_CAP_DESC
+  }
+  const result = await coinGeckoClient.coins.markets({params});
+  return {
+    props: {
+      result
+    },
+  }
+}
